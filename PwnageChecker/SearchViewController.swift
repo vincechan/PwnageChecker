b6ba41a1b88a0c00c3ea4cfedf48f0bf20b7e7b1
Vince Chan <vince.yp.chan@gmail.com>
2016-04-04 00:34:58 -0700

    feat: Redesign and implement the user interface

--- PwnageChecker/SearchViewController.swift
+++ PwnageChecker/SearchViewController.swift
@@ -1,15 +1,20 @@
 //
-//  SearchContactViewController.swift
+//  SearchViewController.swift
 //  PwnageChecker
 //
-//  Created by Vince Chan on 3/17/16.
+//  Created by Vince Chan on 4/2/16.
 //  Copyright © 2016 Vince Chan. All rights reserved.
 //
 
 import UIKit
 import ContactsUI
 
-class SearchContactViewController: UIViewController, CNContactPickerDelegate {
+class SearchViewController: UIViewController, CNContactPickerDelegate {
+    
+    override func viewDidLoad() {
+        ViewHelper.setGradientBackground(view)
+    }
+    
     override func viewWillAppear(animated: Bool) {
         ViewHelper.makeNavBarTransparent(navigationController)
     }
@@ -18,13 +23,36 @@ class SearchContactViewController: UIViewController, CNContactPickerDelegate {
         ViewHelper.undoMakeNavBarTransparent(navigationController)
     }
     
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    @IBAction func enterAccountButtonTouch(sender: AnyObject) {
+        var inputTextField: UITextField?
+        let prompt = UIAlertController(title: "Enter an account", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        prompt.addAction(UIAlertAction(title: "Check", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            // Now do whatever you want with inputTextField (remember to unwrap the optional)
+            if let account = inputTextField?.text {
+                self.checkAccount(account)
+            }
+        }))
+        prompt.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+        prompt.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+            textField.placeholder = "Enter email address or username"
+            inputTextField = textField
+        })
+        
+        presentViewController(prompt, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func selectContactButtonTouch(sender: AnyObject) {
         // show contact picker and configure it with the following behaviors:
         // contacts with 0  email address:   disabled, cannot be selected.
         // contacts with 1  email address:   when selected, the contact's only email address will be used
         // contacts with 1+ email addresses: when selected, the detail of the contact will be presented, and user can select one of the email addresses
         
-         let contactPickerViewController = CNContactPickerViewController()
+        let contactPickerViewController = CNContactPickerViewController()
         
         // disable contacts with no email addresses
         contactPickerViewController.predicateForEnablingContact = NSPredicate(format: "emailAddresses.@count > 0")
@@ -66,7 +94,7 @@ class SearchContactViewController: UIViewController, CNContactPickerDelegate {
     func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
         let account = contact.emailAddresses[0].value as! String
         checkAccount(account)
-    
+        
     }
     
     func contactPicker(picker: CNContactPickerViewController, didSelectContactProperty contactProperty: CNContactProperty) {
--- PwnageChecker/ViewHelper.swift
+++ PwnageChecker/ViewHelper.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewHelper {
     
+    static func gradientBackgroundColor() -> CAGradientLayer {
+        let color1 = UIColor(red: 74/255, green: 74/255, blue: 74/255, alpha: 1)
+        let color2 = UIColor(red: 43/255, green: 43/255, blue: 43/255, alpha: 1)
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [color1.CGColor, color2.CGColor]
+        gradient.locations = [0.0, 1.0]
+        return gradient
+    }
+    
+    static func setGradientBackground(view: UIView) {
+        let gradient = ViewHelper.gradientBackgroundColor()
+        gradient.frame = view.bounds
+        view.layer.insertSublayer(gradient, atIndex: 0)
+    }
+    
     static func showError(error: String) {
         guard let currentMainWindow = UIApplication.sharedApplication().keyWindow else {
             print("No main window.")
@@ -31,4 +46,5 @@ class ViewHelper {
     static func undoMakeNavBarTransparent(navigationController: UINavigationController?) {
         navigationController?.navigationBar.setBackgroundImage(nil, forBarMetrics: .Default)
     }
+    
 }
